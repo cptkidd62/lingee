eec4813d2cc739ff7ff5ee6474aa13b4b54c2c34
cptkidd62 <zelinka15@gmail.com>
2023-10-31 21:10:06 +0100

    create sign in component and add basic routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, provideRouter } from '@angular/router';
+import routeConfig from './routes';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
-  providers: [],
+  providers: [provideRouter(routeConfig)],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
