06d407f6319bd2b5ef8980b92c13380ecc5066e2
cptkidd62 <zelinka15@gmail.com>
2023-11-17 15:56:27 +0100

    login & pwd input fields in signin component

--- client/src/app/signin/signin.component.ts
+++ client/src/app/signin/signin.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent {
-
+  signinForm = new FormGroup({
+    login: new FormControl(''),
+    pwd: new FormControl(''),
+  })
 }
