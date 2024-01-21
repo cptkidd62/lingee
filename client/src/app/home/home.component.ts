af3a18d5980cbed43cd7bc05134d84e0590861d2
cptkidd62 <zelinka15@gmail.com>
2024-01-21 15:40:47 +0100

    add tabs on home page and move reviews to them

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 import { LearnService } from '../_services/learn.service';
 import { TranslateService, TranslateModule } from "@ngx-translate/core";
 
@@ -9,6 +13,10 @@ import { TranslateService, TranslateModule } from "@ngx-translate/core";
   standalone: true,
   imports: [CommonModule,
     RouterLink,
+    MatTabsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
     TranslateModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
@@ -16,10 +24,16 @@ import { TranslateService, TranslateModule } from "@ngx-translate/core";
 export class HomeComponent {
   learnService: LearnService = inject(LearnService)
   revs: { next_review: string, count: number }[] = []
+  srevs: number = 0
 
   constructor(private translate: TranslateService) {
     this.learnService.getReviewsCount().subscribe({
       next: data => this.revs = data
+    })
+    this.learnService.getAllReviewsCount().subscribe({
+      next: data => {
+        this.srevs = data.find(e => e.l_code == localStorage.getItem('currcourse'))?.count || 0
+      }
     })
     translate.setDefaultLang('en');
     translate.use(localStorage.getItem('lang') || 'en');
